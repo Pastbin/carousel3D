e34748eb94588e679888fb8dc5d66e32011bf6b2
Pastbin Alexander <pastbin.alex@gmail.com>
2022-04-19 09:28:23 +0400

    fix bug firefox

--- carousel.js
+++ carousel.js
@@ -5,17 +5,20 @@
   let positionIndex, galleryPosition = 0, currentPositionMouse;
   const carousel = document.querySelector('#carousel')
   const carouselWrap = document.querySelector('#carousel .wrap')
-  const images = document.querySelectorAll("#carousel .image-wrap");
+  const imagesWrap = document.querySelectorAll("#carousel .image-wrap");
+  const images = document.querySelectorAll('#carousel .image')
 
 
-  carouselWrap.addEventListener('mouseup', rotateImageToMe)
+  for (const image of images) {
+    image.addEventListener('mouseup', rotateImageToMe)
+  }
 
+    carousel.addEventListener('mousedown', wrapMouseDownHandler)
 
-  carousel.addEventListener('mousedown', wrapMouseDownHandler)
   window.addEventListener('load', () => {
-      imageWidth = images[0].clientWidth;
-      for (let i = 0; i < images.length; i++) {
-        images[i].style.transform += `rotateY(${-(360 / images.length * i).toFixed(2)}deg) translateZ(${images.length * imageWidth / 6.35}px)`
+      imageWidth = imagesWrap[0].clientWidth;
+      for (let i = 0; i < imagesWrap.length; i++) {
+        imagesWrap[i].style.transform += `rotateY(${-(360 / imagesWrap.length * i).toFixed(2)}deg) translateZ(${imagesWrap.length * imageWidth / 6.35}px)`
       }
     }
   )
@@ -28,26 +31,24 @@
   function wrapMouseDownHandler(e) {
     currentPositionMouse = e.clientX;
     e.preventDefault()
-    // e.target.closest('.wrap').style.transition = "";
+    e.target.closest('.wrap').style.transition = "";
     newPosition = 0;
     currentDownPixel = e.clientX
     carousel.onmousemove = rotateCarousel
     carousel.onmouseup = () => {
       galleryPosition += newPosition;
       carousel.onmousemove = "";
-
     }
 
-    const images = document.querySelectorAll('#carousel .image')
-    images.forEach(imgEl => {
-      if (imgEl !== e) {
-        imgEl.classList.remove('active')
-      }
-    })
+    for (const image of images) {
+        image.classList.remove('active')
+    }
   }
 
   function rotateImageToMe(e) {
-    if (e.type === "mouseup" && currentPositionMouse === e.clientX) {
+    console.log(e.target)
+    let classList = Array.from(e.target.classList).includes('image')
+    if (e.type === "mouseup" && currentPositionMouse === e.clientX && classList) {
       let rotateY = e.target.closest('.image-wrap').style.transform
       rotateY = +rotateY.match(/rotateY\(-?\d{0,5}\.?\d{0,2}/g)?.join("").match(/-?\d{0,5}\.?\d{0,2}/g).join("")
       const diff = Math.abs(galleryPosition % 360 + rotateY);
